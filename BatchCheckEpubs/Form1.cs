b4521bac345aa4cba669d2bbe640f9f89289fac5
dteviot <dteviot@gmail.com>
2020-10-11 22:43:10 +1300

    Batch test all Epubs in a directory

---
+++ BatchCheckEpubs/Form1.cs
@@ -0,0 +1,118 @@
+﻿using MergeWebToEpub;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using UnitTestMergeWebToEpub;
+
+namespace BatchCheckEpubs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new FolderBrowserDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    ScanEpubsInDirectory(dlg.SelectedPath);
+                }
+            }
+        }
+
+        public void ScanEpubsInDirectory(string directory)
+        {
+            var skipList = LogFile.GetSkipList(directory);
+            using (var logFile = new LogFile(directory))
+            {
+                foreach (var fileName in Directory.EnumerateFiles(directory, "*.epub", SearchOption.AllDirectories))
+                {
+                    if (skipList.Contains(fileName))
+                    {
+                        logFile.LogResults(fileName, "Skipped", true);
+                    }
+                    else
+                    {
+                        CheckEpub(fileName, logFile);
+                    }
+                }
+            }
+            MessageBox.Show("Done");
+        }
+
+
+        public void CheckEpub(string fileName, LogFile logFile)
+        {
+            System.Diagnostics.Trace.WriteLine("Checking " + fileName);
+            var epub = new Epub();
+            try
+            {
+                epub.ReadFile(fileName);
+                var errors = epub.ValidateXhtml();
+                if (0 < errors.Count)
+                {
+                    logFile.LogResults(fileName, "Invalid XHTML", false);
+                    return;
+                }
+
+                if (!TestPrettyPrint(epub))
+                {
+                    logFile.LogResults(fileName, "Failed Pretty Print", false);
+                    return;
+                }
+
+                errors = epub.CheckForMissingChapters().ToList();
+                if (0 < errors.Count)
+                {
+                    logFile.LogResults(fileName, "Possibly Missing Chapters", false);
+                    return;
+                }
+
+                errors = epub.ValidateImages().ToList();
+                if (0 < errors.Count)
+                {
+                    logFile.LogResults(fileName, "Webp images", false);
+                    return;
+                }
+            }
+            catch
+            {
+                logFile.LogResults(fileName, "Threw exception", false);
+                return;
+            }
+
+            // if get here, all good
+            logFile.LogResults(fileName, "<none>", true);
+        }
+
+        private bool TestPrettyPrint(Epub epub)
+        {
+            foreach (var item in epub.Opf.Spine)
+            {
+                var xml = Encoding.UTF8.GetString(item.RawBytes);
+                var fromAgility = HtmlAgilityPackUtils.PrettyPrintXhtml(xml);
+                XDocument agilityDoc = XDocument.Parse(fromAgility);
+                XDocument doc = XDocument.Parse(xml);
+                var delta = XmlCompare.ElementSame(agilityDoc.Root, doc.Root);
+                if (!delta.AreSame)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ BatchCheckEpubs/LogFile.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BatchCheckEpubs
+{
+    public class LogFile : IDisposable
+    {
+        public LogFile(string directory)
+        {
+            var backup = Path.Combine(directory, LogFileBackupName);
+            var logFile = Path.Combine(directory, LogFileName);
+
+            File.Delete(backup);
+            if (File.Exists(logFile))
+            {
+                File.Move(logFile, backup);
+            }
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+
+            writer = XmlWriter.Create(logFile, settings);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Epubs");
+        }
+
+
+        public void LogResults(string fileName, string errorType, bool ignore)
+        {
+            writer.WriteStartElement("epub");
+            writer.WriteAttributeString("fileName", fileName);
+            writer.WriteAttributeString("error", errorType);
+            writer.WriteAttributeString("ignore", ignore.ToString());
+            writer.WriteEndElement();
+        }
+
+        public void Dispose()
+        {
+            if (writer != null)
+            {
+                writer.WriteEndElement();
+                writer.Close();
+                writer.Dispose();
+                writer = null;
+            }
+        }
+
+        public static HashSet<string> GetSkipList(string directory)
+        {
+            var skipList = new HashSet<string>();
+            var logFile = Path.Combine(directory, LogFileName);
+            if (File.Exists(logFile))
+            {
+                using (var reader = XmlReader.Create(logFile))
+                {
+                    while (reader.ReadToFollowing("epub"))
+                    {
+                        var ignore = Convert.ToBoolean(reader.GetAttribute("ignore").ToLower());
+                        if (ignore)
+                        {
+                            skipList.Add(reader.GetAttribute("fileName"));
+                        }
+                    }
+                }
+            }
+            return skipList;
+        }
+
+        private XmlWriter writer { get; set; }
+
+
+        private const string LogFileName = "CheckResults.xml";
+        private const string LogFileBackupName = "CheckResults.Backup.xml";
+
+    }
+}
