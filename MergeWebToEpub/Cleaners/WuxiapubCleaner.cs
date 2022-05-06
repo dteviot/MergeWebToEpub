412b8687456284895ac5fda80c11a4df042d8cbb
dteviot <dteviot@gmail.com>
2022-05-06 21:00:34 +1200

    More cleaning of Wuxiapub

--- MergeWebToEpub/Cleaners/WuxiapubCleaner.cs
+++ MergeWebToEpub/Cleaners/WuxiapubCleaner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Xml.Linq;
 using static MergeWebToEpub.CleanerUtils;
 
@@ -17,28 +18,72 @@ namespace MergeWebToEpub
                 return false;
             }
 
-            var toDelete = FindElementsToDelete(doc);
-            foreach (var e in toDelete)
-            {
-                System.Diagnostics.Trace.WriteLine(e.Value);
-            }
+            bool fixedTitle = FixTitle(doc, item);
+
+            var toDelete = FindElementsToDelete(doc, item);
             toDelete.RemoveElements();
-            return 0 < toDelete.Count;
+            return fixedTitle || (0 < toDelete.Count);
         }
 
-        private static List<XText> FindElementsToDelete(XDocument doc)
+        private static bool FixTitle(XDocument doc, EpubItem item)
+        {
+            var header = doc.Root.Descendants(Epub.xhtmlNs + "h1").FirstOrDefault();
+            if (header == null)
+            {
+                return false;
+            }
+            var text = header.Value;
+            var clean = HttpUtility.HtmlDecode(text);
+            bool changed = clean != text;
+            if (changed)
+            {
+                header.Value = clean;
+                System.Diagnostics.Trace.WriteLine($"Changing: {item.Source}");
+                System.Diagnostics.Trace.WriteLine($" ==>  {text}");
+            }
+            return changed;
+        }
+
+        private static List<XText> FindElementsToDelete(XDocument doc, EpubItem item)
         {
             bool ShouldRemoveTextNode(XText text)
             {
-                string rawText = text.Value.Trim();
+                string rawText = StripWhiteSpace(text.Value);
                 return
                     rawText.Equals("Advertisements") ||
-                    rawText.Contains("Read latest Chapters at WuxiaWorld.Site");
+                    rawText.Contains(WuxiaString) ||
+                    rawText.Contains(WebnovelString);
             }
 
-            return  doc.GetTextNodes()
+            var toDelete = doc.GetTextNodes()
                 .Where(ShouldRemoveTextNode)
                 .ToList();
+
+            if (0 < toDelete.Count)
+            {
+                System.Diagnostics.Trace.WriteLine($"Changing: {item.Source}");
+                foreach (var e in toDelete)
+                {
+                    System.Diagnostics.Trace.WriteLine($" ==>  {e.Value}");
+                }
+            }
+            return toDelete;
         }
+
+        private static string StripWhiteSpace(string toStrip)
+        {
+            var sb = new StringBuilder();
+            foreach(var c in toStrip)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        private static readonly string WuxiaString = StripWhiteSpace("Read latest Chapters at WuxiaWorld.S");
+        private static readonly string WebnovelString = StripWhiteSpace("for faster releases read on webnovel");
     }
 }
